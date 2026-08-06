4e086fcd732a3818f103b3c79b449e6be3697f82
ChanakyaTej <chanakyadtej@gmail.com>
2026-08-06 09:22:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (52.11%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
@@ -1,13 +1,11 @@
+from collections import defaultdict
 class Solution:
-    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
-        d={}
+    def groupThePeople(self, a: List[int]) -> List[List[int]]:
+        d=defaultdict(list)
         arr=[]
-        for i in range(len(groupSizes)):
-            if groupSizes[i] not in d:
-                d[groupSizes[i]]=[i]
-            elif groupSizes[i]  in d:
-                d[groupSizes[i]].append(i)
-            if len(d[groupSizes[i]])==groupSizes[i]:
-                arr.append(d[groupSizes[i]])
-                d[groupSizes[i]]=[]
+        for j,i in enumerate(a):
+                d[i].append(j)
+                if len(d[i])==i:
+                    arr.append(d[i])
+                    d[i]=[]
         return arr
